b58665077403b0783bcbc8555f52be5df7aa2b71
ferris <ferrisshang@gmail.com>
2019-12-13 15:13:10 +0800

    Improve win_hid_mk

--- win_hid_mk/build.sh
+++ win_hid_mk/build.sh
@@ -2,5 +2,7 @@ cp -r ../bt_encrypt/alg .
 cp -r ../bt_encrypt/bt_encrypt.? .
 gcc -shared dll.c -o dll.dll -static-libgcc -m32 -Wl,--output-def,libdll.def,--out-implib,libdll.a,--add-stdcall-alias
 gcc main.c hid.c bt_encrypt.c btsnoop_rec.c bt_usb.c \
-	alg/aescrypt.c alg/aeskey.c alg/aestab2.c alg/cmac.c alg/uECC.c \
+	alg/aescrypt.c alg/aeskey.c alg/aestab2.c alg/cmac.c alg/uECC.c alg/ccm.c \
 	-lusb -static-libgcc -mwindows -m32 -L. -std=c99 -o win_hid.exe
+rm -rf alg
+rm -rf bt_encrypt.?
--- win_hid_mk/main.c
+++ win_hid_mk/main.c
@@ -42,7 +42,11 @@ void callback(int nCode, WPARAM wParam, LPARAM lParam)
 			}else{ SEND_MOUSE_EVT(mouse_key, 1, 0, 0); }
 			return;
 		}else if(wParam == PARAM_MOUSE_FUNCD){
+			if(m->mouseData == 0x00010000)mouse_key |= 0x08;
+			else mouse_key |= 0x10;
 		}else if(wParam == PARAM_MOUSE_FUNCU){
+			if(m->mouseData == 0x00010000)mouse_key &= ~0x08;
+			else mouse_key &= ~0x10;
 		}
 		SEND_MOUSE_EVT(mouse_key, mouse_wheel, 0, 0);
 	}else{
@@ -54,7 +58,7 @@ void callback(int nCode, WPARAM wParam, LPARAM lParam)
 		}else{
 			if(k->scanCode == 29) flag |= 0x01; if(k->scanCode == 42) flag |= 0x02; if(k->scanCode == 56) flag |= 0x04;
 			if(k->scanCode == 91) flag |= 0x08; if(k->scanCode == 54) flag |= 0x20; if(k->scanCode == 92) flag |= 0x80;
-			SEND_KEY_EVT(flag, key_map[k->scanCode]);
+			SEND_KEY_EVT(flag, key_map[((k->flags&1)?0x80:0) | k->scanCode]);
 		}
 	}
 }
